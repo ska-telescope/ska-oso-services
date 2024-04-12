6406e5c164d60b7cf911ab2d928a9aae1b7a39e8
Thad Kenny <thaddeus.kenny@stfc.ac.uk>
2024-04-12 12:29:36 +0100

    BTN-2280 refactor validation to make functions more easily testable; add error key to response;

--- src/ska_oso_services/odt/validation.py
+++ src/ska_oso_services/odt/validation.py
@@ -1,3 +1,16 @@
+"""
+This module defines validation functions which are all applied to SBDefinitions.
+
+The functions should all return the same type - a dict of error messages with
+unique keys.
+
+Each function performs an isolated part of the validation,
+though this might still result in more than one message due to the complex
+nature of the validation
+
+They are then all applied to the SBDefinition and the results combined.
+"""
+
 import logging
 from typing import Tuple, Union
 
@@ -6,24 +19,17 @@ from ska_oso_pdm.entities.common.sb_definition import SBDefinition
 from ska_oso_pdm.entities.dish.dish_configuration import ReceiverBand
 from ska_oso_pdm.schemas import CODEC as MARSHMALLOW_CODEC
 
-from ska_oso_services.odt.generated.models.error_response import ErrorResponse
-from ska_oso_services.odt.generated.models.validation_response import ValidationResponse
+from ska_oso_services.common.model import ErrorResponse, ValidationResponse
 
 Response = Tuple[Union[SBDefinition, ValidationResponse, ErrorResponse], int]
 
 LOGGER = logging.getLogger(__name__)
 
 
-def validate_sbd(sbd_str: str) -> dict:
-    messages = []
-    try:
-        sbd = MARSHMALLOW_CODEC.loads(SBDefinition, sbd_str)
-    except (ValidationError, ValueError) as err:
-        messages.append(str(err))
-        return {"validation_errors": messages}
-
+def _validate_csp_and_dish_combination(sbd: SBDefinition) -> dict[str, str]:
     # band_5_tuning should not be present in csp_configuration if dish_configuration
     # receiver band is not 5a or 5b
+    messages = {}
     dish_configs = {dc.dish_configuration_id: dc for dc in sbd.dish_configurations}
     csp_configs = {csp.config_id: csp for csp in sbd.csp_configurations}
     for scan_def in sbd.scan_definitions:
@@ -31,7 +37,7 @@ def validate_sbd(sbd_str: str) -> dict:
         csp_config = None
 
         if scan_def.dish_configuration_id not in dish_configs.keys():
-            messages.append(
+            messages[f"dish_config_not_in_sb_{scan_def.scan_definition_id}"] = (
                 f"Dish configuration '{scan_def.dish_configuration_id}' defined "
                 f"in scan definition '{scan_def.scan_definition_id}' does not "
                 "exist in the SB"
@@ -40,7 +46,7 @@ def validate_sbd(sbd_str: str) -> dict:
             dish_config = dish_configs[scan_def.dish_configuration_id]
 
         if scan_def.csp_configuration_id not in csp_configs.keys():
-            messages.append(
+            messages[f"csp_config_not_in_sb_{scan_def.scan_definition_id}"] = (
                 f"CSP configuration '{scan_def.csp_configuration_id}' defined "
                 f"in scan definition '{scan_def.scan_definition_id}' does not "
                 "exist in the SB"
@@ -54,10 +60,38 @@ def validate_sbd(sbd_str: str) -> dict:
                 and dish_config.receiver_band
                 not in [ReceiverBand.BAND_5A, ReceiverBand.BAND_5B]
             ):
-                messages.append(
+                messages[
+                    f"csp_and_dish_band_mismatch_{scan_def.scan_definition_id}"
+                ] = (
                     f"Scan definition '{scan_def.scan_definition_id}' specifies "
                     "CSP configuration with band_5_tuning but dish configuration "
                     f"with receiver band {dish_config.receiver_band}"
                 )
 
-    return {"validation_errors": messages}
+    return messages
+
+
+VALIDATION_FNS = [_validate_csp_and_dish_combination]
+
+
+def validate_sbd(sbd_str: str) -> dict[str, str]:
+    """
+    Top level validation function for an SBDefinition.
+
+    It applies all the individual validation functions in this module and
+    flattens the results into a single dictionary
+
+    :param sbd_str: SBDefinition as a string to be deserialised and validated
+    :return: a dictionary with individual validation error mesages,
+        each with a unique key which should identify which part of the entity is invalid
+    """
+    try:
+        sbd = MARSHMALLOW_CODEC.loads(SBDefinition, sbd_str)
+    except (ValidationError, ValueError) as err:
+        return {"deserialisation_error": str(err)}
+
+    return {
+        error_key: error_description
+        for single_validation_result in [fn(sbd) for fn in VALIDATION_FNS]
+        for error_key, error_description in single_validation_result.items()
+    }
--- tests/unit/odt/test_validation.py
+++ tests/unit/odt/test_validation.py
@@ -1,63 +1,71 @@
 import json
-from copy import deepcopy
 
-from ska_oso_services.odt.validation import validate_sbd
-from tests.unit.util import VALID_MID_SBDEFINITION_JSON
+from ska_oso_pdm.entities.dish.dish_configuration import ReceiverBand
+
+from ska_oso_services.odt.validation import (
+    _validate_csp_and_dish_combination,
+    validate_sbd,
+)
+from tests.unit.util import VALID_MID_SBDEFINITION_JSON, TestDataFactory
 
 
 def test_valid_sbd_returns_no_messages():
     result = validate_sbd(VALID_MID_SBDEFINITION_JSON)
 
-    assert result["validation_errors"] == []
+    assert result == {}
 
 
-def test_sbd_deserailise_error():
+def test_sbd_deserialise_error():
     result = validate_sbd(json.dumps({"i am": "not a valid sbd"}))
 
-    assert result["validation_errors"] == [
-        "{'interface': ['Missing data for required field.'], 'telescope': ['Missing"
-        " data for required field.']}"
-    ]
+    assert result == {
+        "deserialisation_error": (
+            "{'interface': ['Missing data for required field.'], "
+            "'telescope': ['Missing data for required field.']}"
+        )
+    }
 
 
 def test_config_not_present_error():
-    invalid_sbd = json.loads(deepcopy(VALID_MID_SBDEFINITION_JSON))
+    invalid_sbd = TestDataFactory.sbdefinition()
 
     # dish config specified in scan definition must be present in SB
     fake_dish_conf_id = "dish config abc"
     fake_csp_conf_id = "csp config abc"
-    invalid_sbd["scan_definitions"][0]["dish_configuration"] = fake_dish_conf_id
-    invalid_sbd["scan_definitions"][0]["csp_configuration"] = fake_csp_conf_id
+    invalid_sbd.scan_definitions[0].dish_configuration_id = fake_dish_conf_id
+    invalid_sbd.scan_definitions[0].csp_configuration_id = fake_csp_conf_id
 
-    result = validate_sbd(json.dumps(invalid_sbd))
+    result = _validate_csp_and_dish_combination(invalid_sbd)
 
-    assert result["validation_errors"] == [
-        (
-            "Dish configuration 'dish config abc' defined in scan definition"
+    assert result == {
+        "dish_config_not_in_sb_calibrator scan": (
+            "Dish configuration 'dish config abc' defined in scan definition "
+            "'calibrator scan' does not exist in the SB"
+        ),
+        "csp_config_not_in_sb_calibrator scan": (
+            "CSP configuration 'csp config abc' defined in scan definition"
             " 'calibrator scan' does not exist in the SB"
         ),
-        (
-            "CSP configuration 'csp config abc' defined in scan definition 'calibrator"
-            " scan' does not exist in the SB"
-        ),
-    ]
+    }
 
 
 def test_band_5_tuning():
-    invalid_sbd = json.loads(deepcopy(VALID_MID_SBDEFINITION_JSON))
-    invalid_sbd["dish_configurations"][0]["receiver_band"] = "1"  # Not Band 5a
+    invalid_sbd = TestDataFactory.sbdefinition()
+    invalid_sbd.dish_configurations[
+        0
+    ].receiver_band = ReceiverBand.BAND_1  # Not Band 5a
 
-    result = validate_sbd(json.dumps(invalid_sbd))
+    result = _validate_csp_and_dish_combination(invalid_sbd)
 
-    assert result["validation_errors"] == [
-        (
-            "Scan definition 'calibrator scan' specifies CSP configuration with"
-            " band_5_tuning but dish configuration with receiver band"
-            " ReceiverBand.BAND_1"
+    assert result == {
+        "csp_and_dish_band_mismatch_calibrator scan": (
+            "Scan definition 'calibrator scan' specifies CSP configuration with "
+            "band_5_tuning but dish configuration with "
+            "receiver band ReceiverBand.BAND_1"
         ),
-        (
-            "Scan definition 'science scan' specifies CSP configuration with"
-            " band_5_tuning but dish configuration with receiver band"
-            " ReceiverBand.BAND_1"
+        "csp_and_dish_band_mismatch_science scan": (
+            "Scan definition 'science scan' specifies CSP configuration with "
+            "band_5_tuning but dish configuration with "
+            "receiver band ReceiverBand.BAND_1"
         ),
-    ]
+    }
