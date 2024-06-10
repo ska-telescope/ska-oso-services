a7390943b1a713399275c96fa4442e9c56dd5f0d
Brendan McCollam <brendan.mccollam@stfc.ac.uk>
2024-06-10 12:06:08 +0530

    BTN-2377: Adds Sentry SDK integration

--- src/ska_oso_services/wsgi.py
+++ src/ska_oso_services/wsgi.py
@@ -3,10 +3,26 @@ Entry point for application
 """
 import logging
 
+import sentry_sdk
 from gunicorn import glogging
 from ska_ser_logging import configure_logging, get_default_formatter
 
 from ska_oso_services import create_app
+
+# Comes from https://skao-7j.sentry.io/settings/projects/oso-services/keys/
+SENTRY_DSN = "https://b3fc20cf2b04b13efad0fec41a9682c6@o4507377535287296.ingest.de.sentry.io/4507405976797264"  # fmt: skip # pylint # noqa
+
+# https://docs.sentry.io/platforms/python/integrations/flask/
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 
 app = create_app()
 
