c79736f9ec20a2bb5f4c54ae106c846812549359
Brendan McCollam <brendan.mccollam@stfc.ac.uk>
2024-08-01 14:59:02 +0100

    BTN-2418: Rearrange and modularize route imports

--- src/ska_oso_services/app.py
+++ src/ska_oso_services/app.py
@@ -1,9 +1,7 @@
 """
-ska_oso_services
+ska_oso_services app.py
 """
-
 import os
-from importlib.metadata import version
 from typing import Any, Dict
 
 from fastapi import FastAPI
@@ -14,30 +12,7 @@ from ska_oso_services.common import (
     oda,
     oda_not_found_handler,
 )
-from ska_oso_services.odt.api.prjs import router as projects_router
-from ska_oso_services.odt.api.sbds import router as sbds_router
-
-KUBE_NAMESPACE = os.getenv("KUBE_NAMESPACE", "ska-oso-services")
-OSO_SERVICES_MAJOR_VERSION = version("ska-oso-services").split(".")[0]
-# The base path includes the namespace which is known at runtime
-# to avoid clashes in deployments, for example in CICD
-API_PATH = f"/{KUBE_NAMESPACE}/odt/api/v{OSO_SERVICES_MAJOR_VERSION}"
-
-
-class CustomRequestBodyValidator:  # pylint: disable=too-few-public-methods
-    """
-        There is a (another) issue with Connexion where it cannot validate against a
-        spec with polymorphism, like the SBDefinition.
-    See https://github.com/spec-first/connexion/issues/1569
-    As a temporary hack, this basically turns off the validation
-    """
-
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def __call__(self, function):
-        return function
-
+from ska_oso_services import odt
 
 def create_app(production=True) -> FastAPI:
     """
@@ -52,8 +27,9 @@ def create_app(production=True) -> FastAPI:
         allow_headers=["*"],
         allow_credentials=True,
     )
-    app.include_router(projects_router)
-    app.include_router(sbds_router)
+    # Assemble the constituent APIs:
+    app.include_router(odt.router)
+    # app.include_router(ptt.router)...
     app.exception_handler(KeyError)(oda_not_found_handler)
     if not production:
         app.exception_handler(Exception)(dangerous_internal_server_handler)
