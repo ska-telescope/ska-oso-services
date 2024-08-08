ea2b87102a58aaad8ce604f3eb9fdb4aa139db72
Brendan McCollam <brendan.mccollam@stfc.ac.uk>
2024-08-08 17:59:38 +0100

    Revert "BTN-2418: Mount as subapp"
    This reverts commit a2da597c21e3efcfce4791ed6a87b6bedeccb4ca.

--- src/ska_oso_services/app.py
+++ src/ska_oso_services/app.py
@@ -2,21 +2,6 @@
 ska_oso_services app.py
 """
 
-import os
-from functools import partial
-from importlib.metadata import version
-
-KUBE_NAMESPACE = os.getenv("KUBE_NAMESPACE", "ska-oso-services")
-OSO_SERVICES_MAJOR_VERSION = version("ska-oso-services").split(".")[0]
-# The base path includes the namespace which is known at runtime
-# to avoid clashes in deployments, for example in CICD
-ROUTE_PREFIX = partial(
-    "/{namespace}/{service}/api/v{version}".format,
-    namespace=KUBE_NAMESPACE,
-    version=OSO_SERVICES_MAJOR_VERSION,
-)
-
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -41,8 +26,8 @@ def create_app(production=True) -> FastAPI:
         allow_credentials=True,
     )
     # Assemble the constituent APIs:
-    app.mount(ROUTE_PREFIX(service="odt"), odt.app)
-    # app.mount(ROUTE_PREFIX(service='ptt'), ptt.app)
+    app.include_router(odt.router)
+    # app.include_router(ptt.router)...
     app.exception_handler(KeyError)(oda_not_found_handler)
     if not production:
         app.exception_handler(Exception)(dangerous_internal_server_handler)
--- src/ska_oso_services/odt/__init__.py
+++ src/ska_oso_services/odt/__init__.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+import os
+from importlib.metadata import version
+
+KUBE_NAMESPACE = os.getenv("KUBE_NAMESPACE", "ska-oso-services")
+OSO_SERVICES_MAJOR_VERSION = version("ska-oso-services").split(".")[0]
+# The base path includes the namespace which is known at runtime
+# to avoid clashes in deployments, for example in CICD
+API_PREFIX = f"/{KUBE_NAMESPACE}/odt/api/v{OSO_SERVICES_MAJOR_VERSION}"
+
+from fastapi import APIRouter
 
 from ska_oso_services.odt.api import prjs, sbds
 
-app = FastAPI()
-app.include_router(prjs.router)
-app.include_router(sbds.router)
+router = APIRouter(prefix=API_PREFIX, tags=["ODT"])
+router.include_router(prjs.router)
+router.include_router(sbds.router)
