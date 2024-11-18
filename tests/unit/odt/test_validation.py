781974e2beaef8f47720d696b965034fe230153f
Thad Kenny <thaddeus.kenny@stfc.ac.uk>
2024-11-18 13:42:19 +0000

    BTN-2523 remove validation of dish config from mid

--- tests/unit/odt/test_validation.py
+++ tests/unit/odt/test_validation.py
@@ -1,13 +1,8 @@
 from unittest import mock
 
 from ska_oso_pdm.sb_definition import SBDefinition
-from ska_oso_pdm.sb_definition.dish.dish_configuration import ReceiverBand
 
-from ska_oso_services.odt.validation import (
-    _validate_csp,
-    _validate_csp_and_dish_combination,
-    validate_sbd,
-)
+from ska_oso_services.odt.validation import _validate_csp, validate_sbd
 from tests.unit.util import (
     VALID_LOW_SBDEFINITION_JSON,
     VALID_MID_SBDEFINITION_JSON,
@@ -43,19 +38,12 @@ def test_validate_runs_functions():
 def test_config_not_present_error():
     invalid_sbd = TestDataFactory.sbdefinition()
 
-    # dish config specified in scan definition must be present in SB
-    fake_dish_conf_id = "dish config abc"
     fake_csp_conf_id = "csp config abc"
-    invalid_sbd.scan_definitions[0].dish_configuration_ref = fake_dish_conf_id
     invalid_sbd.scan_definitions[0].csp_configuration_ref = fake_csp_conf_id
 
-    result = _validate_csp_and_dish_combination(invalid_sbd)
+    result = _validate_csp(invalid_sbd)
 
     assert result == {
-        "dish_config_not_in_sb_calibrator scan": (
-            "Dish configuration 'dish config abc' defined in scan definition "
-            "'calibrator scan' does not exist in the SB"
-        ),
         "csp_config_not_in_sb_calibrator scan": (
             "CSP configuration 'csp config abc' defined in scan definition"
             " 'calibrator scan' does not exist in the SB"
@@ -75,27 +63,5 @@ def test_low_config_not_present_error():
         "csp_config_not_in_sb_science": (
             "CSP configuration 'csp config abc' defined in scan definition"
             " 'science' does not exist in the SB"
-        ),
-    }
-
-
-def test_band_5_tuning():
-    invalid_sbd = TestDataFactory.sbdefinition()
-    invalid_sbd.dish_configurations[0].receiver_band = (
-        ReceiverBand.BAND_1
-    )  # Not Band 5a
-
-    result = _validate_csp_and_dish_combination(invalid_sbd)
-
-    assert result == {
-        "csp_and_dish_band_mismatch_calibrator scan": (
-            "Scan definition 'calibrator scan' specifies CSP configuration with "
-            "band_5_tuning but dish configuration with "
-            "receiver band ReceiverBand.BAND_1"
-        ),
-        "csp_and_dish_band_mismatch_science scan": (
-            "Scan definition 'science scan' specifies CSP configuration with "
-            "band_5_tuning but dish configuration with "
-            "receiver band ReceiverBand.BAND_1"
         ),
     }
