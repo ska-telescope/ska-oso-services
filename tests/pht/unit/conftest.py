3c56c20b0a7eaec8c4b385a45a39297e10501f3b
Tonye Irabor <janesoki@Tonyes-MacBook-Pro.local>
2025-02-10 17:58:14 +0000

    STAR-421: mile checking ...

--- tests/pht/unit/conftest.py
+++ tests/pht/unit/conftest.py
@@ -1,36 +1,22 @@
 """
 pytest fixtures to be used by unit tests
 """
+
+from importlib.metadata import version
+
 import pytest
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
 
-from ska_oso_pht_services import create_app, resolve_openapi_spec
+from ska_oso_services import create_app
 
-
-@pytest.fixture(scope="module")
-def spec():
-    """
-    Module scoped fixture so $refs are only resolved once
-    """
-    return resolve_openapi_spec()
+OSO_SERVICES_MAJOR_VERSION = version("ska-oso-services").split(".")[0]
+PHT_BASE_API_URL = f"/ska-oso-services/oso/api/v{OSO_SERVICES_MAJOR_VERSION}/pht"
 
 
 @pytest.fixture()
-def test_app(spec):  # pylint: disable=redefined-outer-name
-    """
-    Fixture to configure a test app instance
-    """
-    connexion = create_app(spec)
-    connexion.app.config.update(
-        {
-            "TESTING": True,
-        }
-    )
-    yield connexion.app
-
-
-@pytest.fixture()
-def client(test_app):  # pylint: disable=redefined-outer-name
+def client(test_app: FastAPI) -> TestClient:
     """
     Create a test client from the app instance, without running a live server
     """
-    return test_app.test_client()
+    return TestClient(test_app)
