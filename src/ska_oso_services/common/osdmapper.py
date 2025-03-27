a7f754fc513582f097e087f5e101046dcfecc6b0
Liz Bartlett <liz.bartlett@stfc.ac.uk>
2025-03-27 21:41:30 +0000

    BTN-2569 extending osdmapper to have a dataclass for the subarrays, and looping over both AA0.5 and AA1 so that configuration_get() returns subarray info

--- src/ska_oso_services/common/osdmapper.py
+++ src/ska_oso_services/common/osdmapper.py
@@ -10,6 +10,11 @@ from ska_ost_osd.rest.api.resources import get_osd
 
 from ska_oso_services.common.model import AppModel
 
+# We currently only return frequency bands that do not depend on the array
+# assembly, so we just use AA0.5 as a constant. As the configuration API
+# expands we will need to return results for different assemblys.
+SUPPORTED_ARRAY_ASSEMBLIES = ["AA0.5", "AA1"]
+
 
 @dataclasses.dataclass
 class FrequencyBand:
@@ -18,23 +23,25 @@ class FrequencyBand:
     rx_id: Optional[str] = None
 
 
+@dataclasses.dataclass
+class Subarray:
+    name: str
+    stations: list[str | int]
+
+
 class MidConfiguration(AppModel):
     frequency_band: list[FrequencyBand]
+    subarrays: list[Subarray]
 
 
 class LowConfiguration(AppModel):
     frequency_band: FrequencyBand
+    subarrays: list[Subarray]
 
 
 class Configuration(AppModel):
     ska_mid: MidConfiguration
     ska_low: LowConfiguration
-
-
-# We currently only return frequency bands that do not depend on the array
-# assembly, so we just use AA0.5 as a constant. As the configuration API
-# expands we will need to return results for different assemblys.
-ARRAY_ASSEMBLY = "AA0.5"
 
 
 def configuration_from_osd() -> Configuration:
@@ -54,22 +61,50 @@ def configuration_from_osd() -> Configuration:
 
 
 def _get_mid_telescope_configuration() -> MidConfiguration:
-    mid_response = get_osd(
-        array_assembly=ARRAY_ASSEMBLY, capabilities="mid", source="car"
-    )
+
+    subarrays = []
+    for array_assembly in SUPPORTED_ARRAY_ASSEMBLIES:
+        mid_response = get_osd(
+            array_assembly=array_assembly, capabilities="mid", source="car"
+        )
+        subarrays.append(
+            Subarray(
+                name=array_assembly,
+                stations=mid_response["capabilities"]["mid"][array_assembly][
+                    "number_dish_ids"
+                ],
+            )
+        )
+
     receiver_information = mid_response["capabilities"]["mid"]["basic_capabilities"][
         "receiver_information"
     ]
     return MidConfiguration(
         frequency_band=[
             FrequencyBand(**reciever_info) for reciever_info in receiver_information
-        ]
+        ],
+        subarrays=subarrays,
     )
 
 
 def _get_low_telescope_configuration() -> LowConfiguration:
-    low_response = get_osd(
-        array_assembly=ARRAY_ASSEMBLY, capabilities="low", source="car"
-    )
+
+    subarrays = []
+    for array_assembly in SUPPORTED_ARRAY_ASSEMBLIES:
+        low_response = get_osd(
+            array_assembly=array_assembly, capabilities="low", source="car"
+        )
+        subarrays.append(
+            Subarray(
+                name=array_assembly,
+                stations=low_response["capabilities"]["low"][array_assembly][
+                    "number_station_ids"
+                ],
+            )
+        )
+
     receiver_information = low_response["capabilities"]["low"]["basic_capabilities"]
-    return LowConfiguration(frequency_band=FrequencyBand(**receiver_information))
+
+    return LowConfiguration(
+        frequency_band=FrequencyBand(**receiver_information), subarrays=subarrays
+    )
