18fc94d81a3ea131bc53e26f91a2b89dc0b6f126
viivi.pursiainen <viivi.pursiainen@stfc.ac.uk>
2025-04-07 13:11:44 +0100

    Test with API version directly from env variable

--- tests/component/__init__.py
+++ tests/component/__init__.py
@@ -1,11 +1,5 @@
 import os
 
-from ska_oso_services.app import OSO_SERVICES_MAJOR_VERSION
-
 KUBE_NAMESPACE = os.getenv("KUBE_NAMESPACE", "ska-oso-services")
-OSO_SERVICES_URL = os.getenv(
-    "OSO_SERVICES_URL",
-    "http://ska-oso-services-rest-test:5000"
-    f"/{KUBE_NAMESPACE}/oso/api/v{OSO_SERVICES_MAJOR_VERSION}",
-)
+OSO_SERVICES_URL = os.environ["OSO_SERVICES_URL"]
 ODT_URL = f"{OSO_SERVICES_URL}/odt"
