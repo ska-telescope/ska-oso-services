a81cf9e23cd0ed20d37f93569b835649512b8363
Tonye Irabor <tonye.irabor@akkodis.com>
2025-05-23 18:19:54 +0100

    STAR-1322: unlocked coordinates endpoint

--- src/ska_oso_services/app.py
+++ src/ska_oso_services/app.py
@@ -79,7 +79,3 @@ def create_app(production=PRODUCTION) -> FastAPI:
 
 main = create_app()
 oda.init_app(main)
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("app:main", host="127.0.0.1", port=8000, reload=True)
