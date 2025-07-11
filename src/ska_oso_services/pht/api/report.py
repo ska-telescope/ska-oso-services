bf96d1874d1208e1edea690f8ab6afd2a9cc49aa
Tonye Irabor <tonye.irabor@akkodis.com>
2025-07-11 09:19:21 +0100

    STAR-1356: connecting to the database

--- src/ska_oso_services/pht/api/report.py
+++ src/ska_oso_services/pht/api/report.py
@@ -7,6 +7,7 @@ from fastapi import APIRouter, Query
 from ska_oso_services.pht.utils.pht_handler import create_sample_data, join_proposals_panels_reviews_decisions
 from ska_oso_services.common import oda
 from ska_oso_services.pht.model import ProposalReport
+from ska_db_oda.persistence.domain.query import MatchType, UserQuery
 from faker import Faker
 
 fake = Faker()
@@ -21,31 +22,29 @@ router = APIRouter(prefix="/report")
 
 
 
-# @router.get(
-#     "/",
-#     summary="Create a report for admin",
-# )
-# def get_report() -> List[ProposalReviewPanelDecisionRow]:
-#     """
-#     Creates a new proposal in the ODA
-#     """
+@router.get(
+    "/{user_id}",
+    summary="Create a report for admin",
+)
+def get_report(user_id: str,) -> List[ProposalReport]:
+    """
+    Creates a new proposal in the ODA
+    """
 
-#     LOGGER.debug("GET REPORT create")
-#     #get proposals using Andrey's new query -- need to add an endpoint for this
-#     #get all panels
-#     #get all reviews
-#     #get panel decision
-
-  
-#     # with oda.uow() as uow:
-#         # query_param = UserQuery(user=user_id, match_type=MatchType.EQUALS)
-#         # proposals = uow.prsls.query(query_param)
-#         # panels = uow.panels.query(query_param)
-#         # reviews = uow.rvws.query(query_param)
-#         # panel_decisions = uow.pnlds.query(query_param)
-#     proposals, panels, reviews, panel_decisions = create_sample_data()
-#     report = join_proposals_panels_reviews_decisions(proposals, panels, reviews, panel_decisions)
-#     return report 
+    LOGGER.debug("GET REPORT create")
+    #get proposals using Andrey's new query -- need to add an endpoint for this
+    #get all panels
+    #get all reviews
+    #get panel decision
+    LOGGER.debug("GET REPORT")
+    with oda.uow() as uow:
+        query_param = UserQuery(user=user_id, match_type=MatchType.EQUALS)
+        proposals = uow.prsls.query(query_param)
+        panels = uow.panels.query(query_param)
+        reviews = uow.rvws.query(query_param)
+        decisions = uow.pnlds.query(query_param)
+    report = join_proposals_panels_reviews_decisions(proposals, panels, reviews, decisions)
+    return report 
 
 
 @router.get("/", response_model=List[ProposalReport])
@@ -54,8 +53,9 @@ def get_report(
     seed: int | None = Query(None, description="Random seed (for reproducible demo output)")
 ):
     """
-    Get the flattened proposal/panel/review/decision data for the dashboard.
+    Get the flattened proposal/panel/review/decision data for the PHT dashboard.
     """
+   
     # Use the provided seed for reproducibility
     if seed is not None:
         random.seed(seed)
