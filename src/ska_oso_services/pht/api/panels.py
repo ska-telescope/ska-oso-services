1220ef4a69d12f4931cb1bf65efd08c00a26f621
Tonye Irabor <tonye.irabor@akkodis.com>
2025-08-23 18:41:10 +0100

    STAR-1625: proposal update test

--- src/ska_oso_services/pht/api/panels.py
+++ src/ska_oso_services/pht/api/panels.py
@@ -139,21 +139,18 @@ def auto_create_panel(param: PanelCreateRequest) -> str:
         return build_panel_response(panel_objs)
 
 
-@router.get("/{panel_id}/Admin", summary="Retrieve an existing panel by panel_id")
-def get_panel_by_id(
-    panel_id: str,
-    auth: Annotated[
-        AuthContext,
+@router.get("/{panel_id}", summary="Retrieve an existing panel by panel_id",  dependencies=[
         Permissions(
-            roles={Role.OPS_PROPOSAL_ADMIN, Role.SW_ENGINEER},
-            scopes={Scope.PHT_READ},
-        ),
-    ],
+            roles=[Role.OPS_PROPOSAL_ADMIN, Role.SW_ENGINEER], scopes=[Scope.PHT_READ]
+        )
+    ],)
+def get_panel_by_id(
+    panel_id: str
 ) -> Panel:
     logger.debug("GET panel panel_id: %s", panel_id)
 
     with oda.uow() as uow:
-        panel = uow.panels.get(panel_id, auth.user_id)  # pylint: disable=no-member
+        panel = uow.panels.get(panel_id)  # pylint: disable=no-member
     logger.info("Panel retrieved successfully: %s", panel_id)
     return panel
 
@@ -205,7 +202,7 @@ def update_panel(panel_id: str, param: Panel) -> str:
 
 
 @router.get(
-    "/list/{user_id}",
+    "/users/{user_id}/panels",
     summary="Get all panels matching the given query parameters",
     dependencies=[
         Permissions(
