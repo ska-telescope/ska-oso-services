8e9549613c2780a7fd4a784eade910148d365879
Liz Bartlett <liz.bartlett@stfc.ac.uk>
2025-10-24 15:59:50 +0100

    BTN-2877 adding a dummy table to the conftest for testing

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -4,8 +4,11 @@ pytest fixtures to be used by unit tests
 
 from functools import partial
 from importlib.metadata import version
+from pathlib import Path
 
 import pytest
+from astropy.io import ascii
+from astropy.table import QTable
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from ska_aaa_authhelpers.roles import Role
@@ -19,6 +22,17 @@ OSO_SERVICES_MAJOR_VERSION = version("ska-oso-services").split(".")[0]
 APP_BASE_API_URL = f"/ska-oso-services/oso/api/v{OSO_SERVICES_MAJOR_VERSION}"
 ODT_BASE_API_URL = f"{APP_BASE_API_URL}/odt"
 PHT_BASE_API_URL = f"{APP_BASE_API_URL}/pht"
+
+
+@pytest.fixture(scope="module")
+def dummy_calibrator_table() -> QTable:
+    """
+    Fixture to create a dummy table so the tests won't fail if a new calibrator is added
+    """
+    path_to_test_table = (
+        Path(__file__).parents[0] / "files" / "dummy_calibrator_table.ecsv"
+    )
+    return ascii.read(path_to_test_table)
 
 
 @pytest.fixture(scope="module", name="test_app")
