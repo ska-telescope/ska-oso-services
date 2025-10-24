7e91c235ee1f02f0b2c1c768885ee343b70aad82
Liz Bartlett <liz.bartlett@stfc.ac.uk>
2025-10-24 15:35:02 +0100

    BTN-2877 tweaking source code - reading in with astropy.ascii rather than QTable, adding some error handling

--- src/ska_oso_services/common/calibrators.py
+++ src/ska_oso_services/common/calibrators.py
@@ -5,18 +5,22 @@ Module that returns the calibrators
 from pathlib import Path
 from typing import List
 
+from astropy.io import ascii
 from astropy.table import QTable
-from ska_oso_pdm import ICRSCoordinates, Target
+from ska_oso_pdm import ICRSCoordinates, RadialVelocity, Target
 
 CALIBRATOR_TABLE_PATH = Path(__file__).parents[0] / "static" / "calibrator_table.ecsv"
 
-calibrator_table = QTable.read(CALIBRATOR_TABLE_PATH)
+calibrator_table = ascii.read(CALIBRATOR_TABLE_PATH)
 
 
 def to_pdm_target(table: QTable) -> List[Target]:
     """
     function to return a list of PDM Target from an AstroPy QTable
     """
+    if not table:
+        raise ValueError("No calibrators found that match the criteria.")
+
     targets = [
         Target(
             target_id=row["target_id"],
@@ -24,6 +28,9 @@ def to_pdm_target(table: QTable) -> List[Target]:
             reference_coordinate=ICRSCoordinates(
                 ra_str=str(row["ra"].to_string(unit="hourangle", sep=":", pad=True)),
                 dec_str=str(row["dec"].to_string(unit="deg", sep=":", pad=True)),
+            ),
+            radial_velocity=RadialVelocity(
+                redshift=row["redshift"],
             ),
         )
         for row in table
