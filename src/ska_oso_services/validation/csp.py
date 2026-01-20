56199f8ccab2bcea041200969c9235d565885f50
Liz Bartlett <liz.bartlett@stfc.ac.uk>
2026-01-20 16:42:17 +0000

    BTN-2994 check point commit

--- src/ska_oso_services/validation/csp.py
+++ src/ska_oso_services/validation/csp.py
@@ -1,29 +1,25 @@
 # https://stackoverflow.com/a/50099819
-# pylint ignore
+# pylint: disable=no-member
 import math
 
 from astropy import units as u
-from astropy.cosmology import available
 from astropy.units import Quantity
 from ska_oso_pdm import TelescopeType
 from ska_oso_pdm.sb_definition import CSPConfiguration
-from ska_oso_pdm.sb_definition.csp.midcbf import Band5bSubband as pdm_Band5bSubband
-from ska_oso_pdm.sb_definition.csp.midcbf import (
-    CorrelationSPWConfiguration,
-    ReceiverBand,
-)
+from ska_oso_pdm.sb_definition.csp.lowcbf import Correlation
+from ska_oso_pdm.sb_definition.csp.midcbf import CorrelationSPWConfiguration
 
-from ska_oso_services.common.osdmapper import (
-    Band5bSubband,
-    FrequencyBand,
-    configuration_from_osd,
-)
 from ska_oso_services.common.static.constants import (
-    FREQUENCY_SLICE_BANDWIDTH,
-    LOW_STATION_CHANNEL_WIDTH,
+    LOW_CONTINUUM_CHANNEL_WIDTH,
+    LOW_MAXIMUM_FREQUENCY,
+    LOW_MINIMUM_FREQUENCY,
     MID_CHANNEL_WIDTH,
+    MID_FREQUENCY_SLICE_BANDWIDTH,
 )
-from ska_oso_services.validation import OSD, get_subarray_specific_parameter_from_osd
+from ska_oso_services.validation import (
+    get_mid_frequency_band_data_from_osd,
+    get_subarray_specific_parameter_from_osd,
+)
 from ska_oso_services.validation.model import (
     ValidationContext,
     ValidationIssue,
@@ -32,7 +28,6 @@ from ska_oso_services.validation.model import (
     validate,
     validator,
 )
-from ska_oso_services.validation.target import validate_target
 
 
 @validator
@@ -55,15 +50,37 @@ def validate_csp(
 
 
 @validator
+def validate_low_spws(
+    csp_context: ValidationContext[CSPConfiguration],
+) -> list[ValidationIssue]:
+    lowcbf = csp_context.primary_entity.lowcbf
+
+    spws_validation_results = [
+        issue
+        for index, spw in enumerate(lowcbf.correlation_spws)
+        for issue in validate_low_spw(
+            ValidationContext(
+                primary_entity=spw,
+                source_jsonpath=f"$.lowcbf.correlation_spws[{index}]",
+                telescope=csp_context.telescope,
+                array_assembly=csp_context.array_assembly,
+            )
+        )
+    ]
+
+    return spws_validation_results
+
+
+@validator
 def validate_mid_spws(
     csp_context: ValidationContext[CSPConfiguration],
 ) -> list[ValidationIssue]:
     midcbf = csp_context.primary_entity.midcbf
 
     if midcbf.band5b_subband is None:
-        band_data = get_mid_frequency_band_info(midcbf.frequency_band)
+        band_data = get_mid_frequency_band_data_from_osd(midcbf.frequency_band)
     else:
-        band_data = get_mid_frequency_band_info(
+        band_data = get_mid_frequency_band_data_from_osd(
             midcbf.frequency_band, midcbf.band5b_subband
         )
 
@@ -78,9 +95,7 @@ def validate_mid_spws(
                 source_jsonpath=f"$.midcbf.subbands[0].correlation_spws[{index}]",
                 relevant_context={
                     "band_data_from_osd": band_data,
-                    "subband_frequency_slice_offset": midcbf.subbands[
-                        0
-                    ].frequency_slice_offset,
+                    "subband_frequency_slice_offset": midcbf.subbands[0].frequency_slice_offset,
                 },
                 telescope=csp_context.telescope,
                 array_assembly=csp_context.array_assembly,
@@ -89,6 +104,19 @@ def validate_mid_spws(
     ]
 
     return spws_validation_results
+
+
+@validator
+def validate_low_spw(
+    spw_context: ValidationContext[Correlation],
+) -> list[ValidationIssue]:
+    validators = [
+        validate_low_spw_centre_frequency,
+        validate_continuum_spw_bandwidth,
+        validate_low_spw_window,
+    ]
+
+    return validate(spw_context, validators)
 
 
 @validator
@@ -104,12 +132,30 @@ def validate_mid_spw(
 
     validators = [
         validate_mid_spw_centre_frequency,
-        validate_mid_spw_bandwidth,
+        validate_continuum_spw_bandwidth,
         validate_mid_spw_window,
         validate_mid_fsps,
     ]
 
     return validate(spw_context, validators)
+
+
+@validator
+def validate_low_spw_centre_frequency(
+    spw_context: ValidationContext[Correlation],
+) -> list[ValidationIssue]:
+
+    centre_frequency_hz = spw_context.primary_entity.centre_frequency
+
+    if centre_frequency_hz < LOW_MINIMUM_FREQUENCY or centre_frequency_hz > LOW_MAXIMUM_FREQUENCY:
+        return [
+            ValidationIssue(
+                level=ValidationIssueType.ERROR,
+                message=f"Centre frequency of "
+                f"spectral window {centre_frequency_hz}"
+                f" is outside of the telescope capabilities",
+            )
+        ]
 
 
 @validator
@@ -120,11 +166,11 @@ def validate_mid_spw_centre_frequency(
     function to validate an individual MID spectral window central frequency
 
     """
-    centre_frequency = spw_context.primary_entity.centre_frequency
+    centre_frequency_hz = spw_context.primary_entity.centre_frequency
     band_data = spw_context.relevant_context["band_data_from_osd"]
     if (
-        centre_frequency > band_data.max_frequency_hz
-        or centre_frequency < band_data.min_frequency_hz
+        centre_frequency_hz > band_data.max_frequency_hz
+        or centre_frequency_hz < band_data.min_frequency_hz
     ):
         if band_data.sub_bands:
             band_id = "Band5b subband " + str(band_data.sub_band)
@@ -135,15 +181,15 @@ def validate_mid_spw_centre_frequency(
             ValidationIssue(
                 level=ValidationIssueType.ERROR,
                 message=f"Centre frequency of "
-                        f"spectral window {centre_frequency}"
-                        f" is outside of {band_id}",
+                f"spectral window {centre_frequency_hz} Hz"
+                f" is outside of {band_id}",
             )
         ]
 
 
 @validator
-def validate_mid_spw_bandwidth(
-    spw_context: ValidationContext[CorrelationSPWConfiguration],
+def validate_continuum_spw_bandwidth(
+    spw_context: ValidationContext[CorrelationSPWConfiguration] | ValidationContext[Correlation],
 ) -> list[ValidationIssue]:
     """
     function to validate the bandwidth
@@ -156,15 +202,35 @@ def validate_mid_spw_bandwidth(
         * u.Hz
     )
 
-    spw_bandwidth = _calculate_spw_bandwidth(spw_context)
+    spw_bandwidth = _calculate_continuum_spw_bandwidth(spw_context)
 
     if spw_bandwidth > available_bandwidth:
         return [
             ValidationIssue(
                 level=ValidationIssueType.ERROR,
-                message=f"Bandwidth of spectral window {float(spw_bandwidth.to('MHz').value)} MHz is outside "
-                f"of available bandwidth {float(available_bandwidth.to('MHz').value)} Hz for array assembly "
-                f"{spw_context.array_assembly.value}",
+                message=f"Bandwidth of spectral window {float(spw_bandwidth.to('MHz').value)} MHz"
+                " is outside of available bandwidth"
+                " {float(available_bandwidth.to('MHz').value)} Hz for "
+                f"{spw_context.telescope} {spw_context.array_assembly.value}",
+            )
+        ]
+
+
+@validator
+def validate_low_spw_window(
+    spw_context: ValidationContext[Correlation],
+) -> list[ValidationIssue]:
+    centre_frequency = spw_context.primary_entity.centre_frequency * u.Hz
+    spw_bandwidth = _calculate_continuum_spw_bandwidth(spw_context)
+
+    if (centre_frequency + 0.5 * spw_bandwidth) > LOW_MAXIMUM_FREQUENCY or (
+        centre_frequency - 0.5 * spw_bandwidth
+    ) < LOW_MINIMUM_FREQUENCY:
+
+        return [
+            ValidationIssue(
+                level=ValidationIssueType.ERROR,
+                message="Spectral window is outside allowed range",
             )
         ]
 
@@ -175,7 +241,7 @@ def validate_mid_spw_window(
 ) -> list[ValidationIssue]:
 
     centre_frequency = spw_context.primary_entity.centre_frequency * u.Hz
-    spw_bandwidth = _calculate_spw_bandwidth(spw_context)
+    spw_bandwidth = _calculate_continuum_spw_bandwidth(spw_context)
 
     band_data = spw_context.relevant_context["band_data_from_osd"]
 
@@ -200,22 +266,20 @@ def validate_mid_fsps(
     """
 
     centre_frequency = spw_context.primary_entity.centre_frequency * u.Hz
-    spw_bandwidth = _calculate_spw_bandwidth(spw_context)
+    spw_bandwidth = _calculate_continuum_spw_bandwidth(spw_context)
 
-    frequency_offset = spw_context.relevant_context[
-        "frequency_offset"
-    ]  # should be zero
+    frequency_offset = spw_context.relevant_context["frequency_offset"]  # should be zero
 
     minimum_spw_frequency = centre_frequency - 0.5 * spw_bandwidth
     maximum_spw_frequency = centre_frequency + 0.5 * spw_bandwidth
 
     coarse_channel_low = math.floor(
-        (minimum_spw_frequency - frequency_offset + (0.5 * FREQUENCY_SLICE_BANDWIDTH))
-        / FREQUENCY_SLICE_BANDWIDTH
+        (minimum_spw_frequency - frequency_offset + (0.5 * MID_FREQUENCY_SLICE_BANDWIDTH))
+        / MID_FREQUENCY_SLICE_BANDWIDTH
     )
     coarse_channel_high = math.floor(
-        (maximum_spw_frequency - frequency_offset + (0.5 * FREQUENCY_SLICE_BANDWIDTH))
-        / FREQUENCY_SLICE_BANDWIDTH
+        (maximum_spw_frequency - frequency_offset + (0.5 * MID_FREQUENCY_SLICE_BANDWIDTH))
+        / MID_FREQUENCY_SLICE_BANDWIDTH
     )
 
     n_fsps = coarse_channel_high - coarse_channel_low + 1
@@ -228,51 +292,30 @@ def validate_mid_fsps(
         return [
             ValidationIssue(
                 level=ValidationIssueType.ERROR,
-                message="Number of FSPs required for spectral window is greater than the number of FSPs available "
-                f"for array assembly {spw_context.array_assembly}",
+                message="Number of FSPs required for spectral window is greater than the number"
+                f" of FSPs available for array assembly {spw_context.array_assembly}",
             )
         ]
 
 
-def _calculate_spw_bandwidth(
-    spw_context: ValidationContext[CorrelationSPWConfiguration],
-) -> tuple[Quantity, Quantity]:
+def _calculate_continuum_spw_bandwidth(
+    spw_context: ValidationContext[CorrelationSPWConfiguration] | ValidationContext[Correlation],
+) -> Quantity:
     """
-    private function to calculate the minimum and maximum frequencies of spectral windows
+    private function to calculate the bandwidth of spectral windows
     """
 
     number_of_channels = spw_context.primary_entity.number_of_channels
+    # this is currently always zero for both MID and LOW
     zoom_factor = spw_context.primary_entity.zoom_factor
+    if zoom_factor != 0:
+        raise ValueError("zoom windows are not yet supported")
 
-    channel_bandwidth = MID_CHANNEL_WIDTH / (2**zoom_factor)
-    spw_bandwidth = channel_bandwidth * number_of_channels
+    if spw_context.telescope == TelescopeType.SKA_MID:
+        channel_width = MID_CHANNEL_WIDTH
+    else:
+        channel_width = LOW_CONTINUUM_CHANNEL_WIDTH
+
+    spw_bandwidth = channel_width * number_of_channels
 
     return spw_bandwidth
-
-
-def get_mid_frequency_band_info(
-    obs_band: ReceiverBand, band5b_subband: pdm_Band5bSubband | None = None
-) -> FrequencyBand | Band5bSubband:
-    if obs_band != ReceiverBand.BAND_5B and band5b_subband is not None:
-        raise ValueError(f"cannot specify and band 5b subband for band {obs_band}")
-    elif obs_band == ReceiverBand.BAND_5B and band5b_subband is None:
-        raise ValueError(f"band 5b subband must be specified for band 5b observation")
-
-    bands = OSD["ska_mid"].frequency_band
-
-    if band5b_subband is None:
-        band_info = next(
-            band for band in bands if band.rx_id == "Band_" + obs_band.value
-        )
-
-    else:
-        subbands_info = next(
-            band for band in bands if band.rx_id == "Band_" + obs_band.value
-        ).band5b_subbands
-        band_info = next(
-            subband
-            for subband in subbands_info
-            if subband.sub_band == band5b_subband.value
-        )
-
-    return band_info
