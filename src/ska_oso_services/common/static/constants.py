8813a8d46d0a845fb5921c23c99cef5b254b941b
Liz Bartlett <liz.bartlett@stfc.ac.uk>
2026-01-22 15:49:27 +0000

    BTN-2994 turning all constants into functions so they are only computed at runtime not import time, propagating through the code

--- src/ska_oso_services/common/static/constants.py
+++ src/ska_oso_services/common/static/constants.py
@@ -1,5 +1,8 @@
 # pylint: disable=no-member
+from functools import lru_cache
+
 from astropy import units as u
+from astropy.units import Quantity
 from ska_oso_pdm import TelescopeType, ValidationArrayAssembly
 
 from ska_oso_services.validation import (
@@ -7,36 +10,69 @@ from ska_oso_services.validation import (
     get_subarray_specific_parameter_from_osd,
 )
 
-# for both AA0.5 and AA1 this list has one element
-# this is a list for AA2 and is something to do  with
-# how MID is handling zoom windows. The MID and LOW
-# OSD have diverged in this respect - we should probably
-# address this before we support zooms
+
+@lru_cache(maxsize=None)
+def mid_channel_width() -> Quantity:
+    """
+    for both AA0.5 and AA1 this list has one element this is a list for AA2 and
+    is something to do  with how MID is handling zoom windows. The MID and LOW
+    OSD have diverged in this respect - we should probably address this before we
+    support zooms
+    """
+    return (
+        get_subarray_specific_parameter_from_osd(
+            TelescopeType.SKA_MID,
+            ValidationArrayAssembly.AA05,
+            "allowed_channel_width_values_hz",
+        )[0]
+        * u.Hz
+    )
 
 
-MID_CHANNEL_WIDTH = (
-    get_subarray_specific_parameter_from_osd(
-        TelescopeType.SKA_MID,
-        ValidationArrayAssembly.AA05,
-        "allowed_channel_width_values_hz",
-    )[0]
-    * u.Hz
-)
+@lru_cache(maxsize=None)
+def mid_frequency_slice_bandwidth() -> Quantity:
+    """
+    These are CSP constants taken from the CSP source code. Common sample rate for all
+     receptor data streams, achieved after Resampling & Delay Tracking  (RDT) [Hz];
+     applies for all function modes except VLBI
+    """
+    COMMON_SAMPLE_RATE = 220200960 * u.Hz
+    VCC_OVERSAMPLING_FACTOR = 10 / 9
+    return COMMON_SAMPLE_RATE / VCC_OVERSAMPLING_FACTOR
 
 
-LOW_MINIMUM_FREQUENCY = get_low_basic_capability_parameter_from_osd("min_frequency_hz") * u.Hz
-LOW_MAXIMUM_FREQUENCY = get_low_basic_capability_parameter_from_osd("max_frequency_hz") * u.Hz
-LOW_STATION_CHANNEL_WIDTH = (
-    get_low_basic_capability_parameter_from_osd("coarse_channel_width_hz") * u.Hz
-)
+@lru_cache(maxsize=None)
+def low_minimum_frequency() -> Quantity:
+    return get_low_basic_capability_parameter_from_osd("min_frequency_hz") * u.Hz
 
-LOW_CHANNEL_BLOCK = 8 * LOW_STATION_CHANNEL_WIDTH  # = 6.25 MHz
-LOW_CONTINUUM_CHANNEL_WIDTH = (
-    LOW_STATION_CHANNEL_WIDTH
-    / get_low_basic_capability_parameter_from_osd("number_continuum_channels_per_coarse_channel")
-)
 
-LOW_PST_CHANNEL_WIDTH = (16 * LOW_STATION_CHANNEL_WIDTH) / 3456
+@lru_cache(maxsize=None)
+def low_maximum_frequency() -> Quantity:
+    return get_low_basic_capability_parameter_from_osd("max_frequency_hz") * u.Hz
+
+
+@lru_cache(maxsize=None)
+def low_station_channel_width() -> Quantity:
+    return get_low_basic_capability_parameter_from_osd("coarse_channel_width_hz") * u.Hz
+
+
+@lru_cache(maxsize=None)
+def low_continuum_channel_width() -> Quantity:
+    return low_station_channel_width() / get_low_basic_capability_parameter_from_osd(
+        "number_continuum_channels_per_coarse_channel"
+    )
+
+
+@lru_cache(maxsize=None)
+def low_pst_channel_width() -> Quantity:
+    return 16 * low_station_channel_width() / 3456
+
+
+MID_DISH_DIAMETER = Quantity(15, u.m)
+LOW_STATION_DIAMETER = Quantity(39, u.m)
+
+
+# Not telescope constants, used for visibility plots
 STEP_SECONDS_DEFAULT_VISIBILITY = 10
 # palette
 T10_COLOURS = {
@@ -45,14 +81,3 @@ T10_COLOURS = {
     "gold": "#EDC948",
     "red": "#CA380B",
 }
-
-MID_DISH_DIAMETER = 15 * u.m
-LOW_STATION_DIAMETER = 39 * u.m
-
-# CSP constants taken from the CSP source code.
-# Common sample rate for all receptor data streams,
-# achieved after Resampling & Delay Tracking  (RDT) [Hz];
-# applies for all function modes except VLBI
-COMMON_SAMPLE_RATE = 220200960 * u.Hz
-VCC_OVERSAMPLING_FACTOR = 10 / 9
-MID_FREQUENCY_SLICE_BANDWIDTH = COMMON_SAMPLE_RATE / VCC_OVERSAMPLING_FACTOR
