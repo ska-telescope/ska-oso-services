858effdc4122ba9023aafbc7a5b1fa8d55c6024e
Liz Bartlett <liz.bartlett@stfc.ac.uk>
2026-02-18 16:55:32 +0000

    BTN-2927 quick test update

--- src/ska_oso_services/validation/mccs.py
+++ src/ska_oso_services/validation/mccs.py
@@ -55,6 +55,7 @@ def validate_number_subarray_beams(
         return [
             ValidationIssue(
                 level=ValidationIssueType.ERROR,
+                field="$.mccs_allocation",
                 message=f"Number of subarray beams {number_subarray_beams} "
                 f"exceeds allowed {allowed_subarray_beams} for {mccs_context.array_assembly}",
             )
@@ -147,6 +148,7 @@ def validate_number_of_pst_beams_per_scan(
             validation_issues.append(
                 ValidationIssue(
                     level=ValidationIssueType.ERROR,
+                    field="$.target",
                     message=f"Number of PST beams {number_pst_beams} for scan {scan.index + 1} "
                     f"exceeds allowed {allowed_number_pst_beams} for "
                     f"{mccs_context.array_assembly}",
@@ -180,6 +182,7 @@ def validate_subarray_beams_per_scan_have_the_same_duration(
             validation_issues.append(
                 ValidationIssue(
                     level=ValidationIssueType.ERROR,
+                    field="$.mccs_allocation.subarray_beams.scan_sequence",
                     message=f"The scan durations for scan {scan.index + 1} "
                     "are not equal for all subarray beams",
                 )
@@ -239,7 +242,6 @@ def validate_station_bandwidth(
                 calculate_continuum_spw_bandwidth(
                     ValidationContext(
                         primary_entity=spw,
-                        source_jsonpath=f"$.subarray_beams[{beam.beam.subarray_beam_id - 1}]",
                         telescope=mccs_context.telescope,
                         array_assembly=mccs_context.array_assembly,
                     )
@@ -257,6 +259,7 @@ def validate_station_bandwidth(
             validation_issues.append(
                 ValidationIssue(
                     level=ValidationIssueType.ERROR,
+                    field="$.csp_configurations",
                     message=f"At least one station is using more bandwidth "
                     f"({max_total_bandwidth.to(u.MHz).value} MHz) than is "
                     f"available ({available_bandwidth.to(u.MHz).value} MHz) "
