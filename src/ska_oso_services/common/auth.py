3b746fd0d3fa15e6bac2cdc9bdf43fc023d19f83
Brendan McCollam <brendan.mccollam@stfc.ac.uk>
2026-06-09 16:34:13 +0100

    BTN-3401: Configure audience values

--- src/ska_oso_services/common/auth.py
+++ src/ska_oso_services/common/auth.py
@@ -5,7 +5,18 @@ from os import getenv
 from ska_aaa_authhelpers import Requires
 from ska_aaa_authhelpers.test_helpers import TEST_ISSUER, TEST_PUBLIC_KEYS
 
-AUDIENCE = getenv("SKA_AUTH_AUDIENCE", "api://e4d6bb9b-cdd0-46c4-b30a-d045091b501b")
+DEFAULT_AUDIENCES = (
+    # We default to "live" because in the case of a misconfiguration
+    # we want to fail-safe to the most-restrictive setting.
+    # Put another way: if someone goofs up, it's better if a dev
+    # environment is accidentally accepting prod tokens than
+    # vice versa
+    "live:pht",
+    "live:odt",
+    # TODO: Remove this MS Entra once all clients are fully migrated to Indigo.
+    "api://e4d6bb9b-cdd0-46c4-b30a-d045091b501b",
+)
+AUDIENCE = getenv("SKA_AUTH_AUDIENCE", DEFAULT_AUDIENCES)
 
 
 # This should never be true in production, because
