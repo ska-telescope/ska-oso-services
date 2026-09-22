d4b2a253df764769b9b3c87bdadca62fa88b0752
viivi.pursiainen <viivi.pursiainen@stfc.ac.uk>
2026-09-22 14:04:21 +0100

    BTN-3524 More detail in OSD error

--- src/ska_oso_services/common/osdmapper.py
+++ src/ska_oso_services/common/osdmapper.py
@@ -297,7 +297,7 @@ def get_osd_cycles():
     try:
         cycle_numbers = get_available_cycles(get_osd_tmdata())
     except (OSDModelError, ValueError) as error:
-        raise OSDError(error)
+        raise OSDError(error) from error
     return {"cycles": sorted(cycle_numbers)}
 
 
